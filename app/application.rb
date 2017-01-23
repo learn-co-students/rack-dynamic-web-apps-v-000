d998d3d663d2a59746ee1210119e876e08cd776d
Scott Young <young.scottw@gmail.com>
2017-01-23 15:28:50 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,18 @@
 class Application
 
   def call(env)
-    resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp = Rack::Response.new  ## Creates new Rack::Response object
+
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)  ## Get three random nums
+    num_3 = Kernel.rand(1..20)
+
+    if num_1==num_2 && num_2==num_3
+      resp.write "You Win"    ## Doesn't execute until resp.finish called below
+    else
+      resp.write "You Lose"   ## See above comment
+    end
+
     resp.finish
   end
 
