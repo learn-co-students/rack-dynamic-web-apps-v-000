dee1e3d7d820057ffce0276b1eeb1c5d21be0ed0
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-02-15 22:52:24 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,23 @@
 class Application
-
+#=========================================================== 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+#=========================================================== 
+    num_1 = Kernel.rand(1..2)
+    num_2 = Kernel.rand(1..2)
+    num_3 = Kernel.rand(1..2)
+    
+    resp.write "-"*13
+    resp.write "\n| #{num_1} | #{num_2} | #{num_3} |\n"
+    resp.write "-"*13
+
+    if num_1 == num_2 && num_2 == num_3
+      resp.write "\nYou Win" 
+    else
+      resp.write "\n  You Lose"
+    end
+#===========================================================    
     resp.finish
   end
-
+#=========================================================== 
 end
