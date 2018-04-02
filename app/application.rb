4aaa41816ec3bb293d95480c892f1f6424251b17
Sam. O <samrey2018@gmail.com>
2018-04-02 02:25:29 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,25 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
+
+# note K is a capital letter
+
+  num_1 = Kernel.rand(1..20)
+  num_2 = Kernel.rand(1..20)
+  num_3 = Kernel.rand(1..20)
+
+
+# the \n indicates it a new line.
+  resp.write "#{num_1}\n"
+  resp.write "#{num_2}\n"
+  resp.write "#{num_3}\n"
+
+  if num_1==num_2 && num_2==num_3
+    resp.write "You Win"
+  else
+    resp.write "You Lose"
   end
 
+  resp.finish  # the app will not run without the finish.
+  end
 end
